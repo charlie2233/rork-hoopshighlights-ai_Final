00c127ad53b227bc8d48fe73cf69845a61a76f0a
CharLEE.cpp <117957000+charlie2233@users.noreply.github.com>
2026-06-09 13:57:30 -0700

    Nudge app page titles right and up

--- ios/HoopsClips/HoopsClips/HoopsClipsApp.swift
+++ ios/HoopsClips/HoopsClips/HoopsClipsApp.swift
@@ -1,10 +1,13 @@
 import SwiftUI
+import UIKit
 import RevenueCat
 import GoogleSignIn
 
 @main
 struct HoopsClipsApp: App {
     init() {
+        Self.configureNavigationTitlePlacement()
+
         let runtimeConfig = AppRuntimeConfig.shared
         let telemetry = LaunchTelemetry.shared
 
@@ -35,6 +38,36 @@ struct HoopsClipsApp: App {
                 .onOpenURL { url in
                     _ = GIDSignIn.sharedInstance.handle(url)
                 }
+        }
+    }
+
+    private static func configureNavigationTitlePlacement() {
+        let titleOffset = UIOffset(horizontal: 8, vertical: -4)
+        let titleParagraph = NSMutableParagraphStyle()
+        titleParagraph.firstLineHeadIndent = titleOffset.horizontal
+        titleParagraph.headIndent = titleOffset.horizontal
+
+        let titleAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .baselineOffset: 2,
+            .paragraphStyle: titleParagraph
+        ]
+
+        let navigationAppearance = UINavigationBarAppearance()
+        navigationAppearance.configureWithOpaqueBackground()
+        navigationAppearance.backgroundColor = UIColor(AppTheme.darkBg)
+        navigationAppearance.shadowColor = .clear
+        navigationAppearance.titleTextAttributes = titleAttributes
+        navigationAppearance.largeTitleTextAttributes = titleAttributes
+        navigationAppearance.titlePositionAdjustment = titleOffset
+
+        let navigationBar = UINavigationBar.appearance()
+        navigationBar.tintColor = UIColor(AppTheme.neonPurple)
+        navigationBar.standardAppearance = navigationAppearance
+        navigationBar.scrollEdgeAppearance = navigationAppearance
+        navigationBar.compactAppearance = navigationAppearance
+        if #available(iOS 15.0, *) {
+            navigationBar.compactScrollEdgeAppearance = navigationAppearance
         }
     }
 }
